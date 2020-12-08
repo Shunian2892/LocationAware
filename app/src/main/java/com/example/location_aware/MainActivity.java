633de51fd305aad41e10c614d37b5f57200f0175
DESKTOP-EBR7IVA\kimve <kimveldhoen@hotmail.com>
2020-12-08 10:12:20 +0100

    [Added] Map fragment layout and class

--- app/src/main/java/com/example/location_aware/MainActivity.java
+++ app/src/main/java/com/example/location_aware/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.location_aware;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FragmentManager fragmentManager;
+    private MapFragment mapFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Test test
+
+        fragmentManager = getSupportFragmentManager();
+        if(fragmentManager.findFragmentById(R.id.osm_view) == null){
+            mapFragment = new MapFragment();
+            fragmentManager.beginTransaction().add(R.id.osm_view, mapFragment).commit();
+        } else {
+            mapFragment = (MapFragment) fragmentManager.findFragmentById(R.id.osm_view);
+        }
     }
 }
---
+++ app/src/main/java/com/example/location_aware/MapFragment.java
@@ -0,0 +1,54 @@
+package com.example.location_aware;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.views.MapView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link MapFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MapFragment extends Fragment {
+    MapView map;
+
+    public MapFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment MapFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static MapFragment newInstance(String param1, String param2) {
+        MapFragment fragment = new MapFragment();
+
+        return fragment;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_map, container, false);
+
+        map = (MapView) v.findViewById(R.id.osm_view);
+        map.setUseDataConnection(true);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+
+        // Inflate the layout for this fragment
+        return v;
+    }
+}
