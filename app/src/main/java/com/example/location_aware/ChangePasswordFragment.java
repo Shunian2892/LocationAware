e0dd70f08cd1103976cfa94a69ec3030e28a228a
DESKTOP-EBR7IVA\kimve <kimveldhoen@hotmail.com>
2021-01-10 17:06:26 +0100

    [Update] Change password fragment works

--- app/src/main/java/com/example/location_aware/ChangePasswordFragment.java
+++ app/src/main/java/com/example/location_aware/ChangePasswordFragment.java
@@ -10,17 +10,28 @@ import androidx.fragment.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 /**
- * A simple {@link Fragment} subclass.
- * Use the {@link ChangePasswordFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * This class handles the password changes from a user. It takes the current firebase user, the new password from the screen, and then sets the new password in the database.
  */
 public class ChangePasswordFragment extends Fragment {
     private FragmentManager fragmentManager;
     private SettingsFragmentButtons settingsFragmentButtons;
     private ImageButton ibBackButton;
+    private Button confirm;
+    private EditText newPass, repeatNewPass;
+    private FirebaseUser user;
+    private ProgressBar progressBar;
 
     public ChangePasswordFragment() {
         // Required empty public constructor
@@ -37,9 +48,16 @@ public class ChangePasswordFragment extends Fragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_change_password, container, false);
 
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
         fragmentManager = getFragmentManager();
         ibBackButton = v.findViewById(R.id.change_password_backButton);
         settingsFragmentButtons = Data.getInstance().getSettingsButtons();
+
+        confirm = v.findViewById(R.id.change_password_confirm);
+        newPass = v.findViewById(R.id.change_password_newPass);
+        repeatNewPass = v.findViewById(R.id.change_password_newPass_validate);
+        progressBar = v.findViewById(R.id.change_password_progressBar);
 
         return v;
     }
@@ -55,5 +73,38 @@ public class ChangePasswordFragment extends Fragment {
                 fragmentManager.beginTransaction().hide(Data.getInstance().getChangePasswordFragment()).commit();
             }
         });
+
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
+                String newPassword = newPass.getText().toString();
+                String repeatNewPassword = repeatNewPass.getText().toString();
+
+                //Check if the values of both edittexts are equal
+                if(newPassword.equals(repeatNewPassword)){
+                    //If both input strings are equal, update password for current user and show toast message
+                    user.updatePassword(newPassword).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                progressBar.setVisibility(View.GONE);
+                                makeToast(R.string.toast_changePassword);
+                                newPass.setText("");
+                                repeatNewPass.setText("");
+                            }
+                        }
+                    });
+                } else {
+                    //If both input strings aren't equal, then show user a toast message
+                    makeToast(R.string.toast_changePassword_failed);
+                }
+            }
+        });
     }
+
+    private void makeToast(int message){
+        Toast.makeText(getContext(), message, Toast.LENGTH_LONG).show();
+    }
+
 }
