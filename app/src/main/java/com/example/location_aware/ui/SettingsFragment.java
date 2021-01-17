041d81613f50aa3a991a430959b90ac5e47fa6b0
Nathalie Seen <n.seen@student.avans.nl>
2021-01-17 16:26:01 +0100

    [FIX] definitely fixed now (also some laout changes) :)

--- app/src/main/java/com/example/location_aware/ui/SettingsFragment.java
+++ app/src/main/java/com/example/location_aware/ui/SettingsFragment.java
@@ -2,6 +2,7 @@ package com.example.location_aware.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
@@ -46,6 +47,15 @@ public class SettingsFragment extends Fragment {
         fragmentManager.beginTransaction().replace(R.id.settings_fragment_container, settingsButtons).commit();
         return v;
     }
+
+    /*@Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if(savedInstanceState != null){
+            //Data.getInstance().getSettingsFragment();
+            fragmentManager.beginTransaction().replace(R.id.fragment_container, Data.getInstance().getSettingsFragment());
+        }
+    }*/
 
     /**
      * Checks if there is a settings buttons fragment. If there isn't, then make a new SettingsButtonsFragment and commit
